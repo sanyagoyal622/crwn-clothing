83cf10618a2c94c8cdf51dea1362cfc4d7d72b15
sanyagoyal622 <sanyagoyal622@gmail.com>
2020-05-31 01:30:10 +0530

    added user selector in our code and impleneted in components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, Switch,Redirect} from 'react-router-dom'
 import {connect} from 'react-redux';
-
+import {createStructuredSelector} from 'reselect';
 import './App.css';
 import HomePage from'./pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -9,6 +9,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { setCurrentUser} from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 class App extends React.Component {
  
 
@@ -59,8 +60,8 @@ render() {
 }
 }
 
-const mapStateToProps=({user}) =>({
-  currentUser:user.currentUser
+const mapStateToProps=createStructuredSelector({
+  currentUser:selectCurrentUser
 });
 const mapDispatchToProps = dispatch => ({
   setCurrentUser:user=> dispatch (setCurrentUser(user))
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,13 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
+import {createStructuredSelector} from 'reselect';
+
 import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
+import {selectCurrentUser} from '../../redux/user/user.selectors';
 const Header=({currentUser,hidden}) => (
     <div className='header'>
         <Link className="logo-container" to='/'>
@@ -37,9 +40,10 @@ const Header=({currentUser,hidden}) => (
     </div>
 );
 
-const mapStateToProps=({user:{currentUser},cart:{hidden}})=> ({
-    currentUser,
-    hidden
+const mapStateToProps=(state)=> ({
+    currentUser:selectCurrentUser(state),
+
+    hidden:selectCartHidden(state)
 });
 
 export default connect(mapStateToProps) (Header);
