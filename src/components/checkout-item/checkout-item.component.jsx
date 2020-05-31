79f38b11daaf444a50623f610744584ebe520315
sanyagoyal622 <sanyagoyal622@gmail.com>
2020-05-31 08:12:00 +0530

    added remove cart-item feature in checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 
 import './checkout-item.styles.scss';
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
+const CheckoutItem = ({cartItem,clearItem}) => {
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
   <div className='checkout-item'>
     <div className='image-container'>
       <img src={imageUrl} alt='item' />
@@ -10,8 +15,15 @@ const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
     <span className='name'>{name}</span>
     <span className='quantity'>{quantity}</span>
     <span className='price'>{price}</span>
-    <div className='remove-button'>&#10005;</div>
+    <div className='remove-button' onClick={()=>clearItem(cartItem)}>&#10005;</div>
   </div>
 );
+  };
+const mapDispatchToProps=dispatch=> ({
+  clearItem:item=>dispatch(clearItemFromCart(item))
+});
 
-export default CheckoutItem;
+export default connect(
+  null,
+  mapDispatchToProps
+)(CheckoutItem);
