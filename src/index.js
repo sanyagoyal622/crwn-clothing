c5f9fd07262e3a805970dcbbc3cc207a6f182eaf
sanyagoyal622 <sanyagoyal622@gmail.com>
2020-06-03 10:04:56 +0530

    moved our directory and shop data in its own reducer ,crcreated respective selectors and updated corresponding components with new redux flow with directory/shop

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 //browser router is a component from react router dom library
 import {Provider} from 'react-redux';
+import {persistGate} from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
 
-import store from'./redux/store';
+import {store,persistor} from'./redux/store';
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
-    <App />
+  <persistGate persistor={persistor}>
+  <App />
+  </persistGate>
+   
   </BrowserRouter>,
   </Provider>,
   document.getElementById('root')  
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import {combineReducers} from 'redux';
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart.reducer';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import directorytReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
+//we are telling redux that we want local storage
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['cart']
+    //whitelist doesnot contain user because it is handelled by firebase
+}
+//this is an json object telling the persist about the required configs
 
-
-export default combineReducers ({
+const rootReducer= combineReducers ({
     user:userReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    directory:directorytReducer,
+    shop:shopReducer
 });
+
+export default persistReducer(persistConfig,rootReducer);
